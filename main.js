0a710fd220f7acc8e9930a059ecc8d7c814895c6
Vicente Quesada Camón <vicentequesadadeb@gmail.com>
2020-06-07 03:45:20 +0200

    main.js solved

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 function makeArrayConsecutive(nums) {
   //  write code here.
   const numsOrdered = nums.sort();
-  console.log(numsOrdered);
   return missingNumbers(numsOrdered);
 }
 
 function missingNumbers(nums) {
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
-    console.log("actual " + nums[i]);
-  console.log("siguiente " + nums[i+1]);
-    if (nums[i] * -1 + nums[i+1] >= 1) {
-      count += nums[i] * -1 + nums[i+1]-1;
+    if (nums[i] * -1 + nums[i + 1] >= 1) {
+      count += nums[i] * -1 + nums[i + 1] - 1;
     }
   }
   return count;
